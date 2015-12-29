e831e7d4f445a0aa516c16199a586cc35b03a96c
Tang Zijian <tangzj77@gmail.com>
2015-12-29 17:56:25 +0800

    added discover layout

---
+++ ARShop/ARShop/Views/PhotoTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoTableViewCell.swift
+//  ARShop
+//
+//  Created by Tang Zijian on 29/12/15.
+//  Copyright © 2015 sg.edu.nus. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak private var photoView: UIImageView!
+    @IBOutlet weak private var shopNameLabel: UILabel!
+    @IBOutlet weak private var dateTakenLabel: UILabel!
+    @IBOutlet weak private var tagsLabel: UILabel!
+}
---
+++ ARShop/ARShop/Views/ShopTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ShopTableViewCell.swift
+//  ARShop
+//
+//  Created by Tang Zijian on 29/12/15.
+//  Copyright © 2015 sg.edu.nus. All rights reserved.
+//
+
+import UIKit
+
+class ShopTableViewCell: UITableViewCell {
+
+    @IBOutlet weak private var shopLogoView: UIImageView!
+    @IBOutlet weak private var shopNameLabel: UILabel!
+    @IBOutlet weak private var shopCategoryLabel: UILabel!
+}
